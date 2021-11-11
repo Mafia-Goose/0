2e86c699dcfd559199a9e2ab1f6784f5aa139216
Mafia-Goose <78287101+Mafia-Goose@users.noreply.github.com>
2021-11-11 15:32:22 -0500

    python file for testing, might use later

---
+++ start.py
@@ -0,0 +1,19 @@
+import time as tm
+
+def start():
+	print()
+
+
+pt = input('Start? (Y/N) ')
+print()
+if pt == "y" or pt == "Y":
+  tm.sleep(1)
+
+  print('Starting...')
+
+  tm.sleep(1)
+
+  print()
+  print('Started')
+  print()
+  start()
